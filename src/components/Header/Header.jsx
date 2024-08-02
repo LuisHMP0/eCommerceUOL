96af395abbb1f2d1b68f0eff91c5980d1020e692
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2024-08-02 15:24:20 -0300

    feat(ui)> add responsive hamburguer menu for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import login from './img/loginLogo.png'
 import carrinho from './img/carrinhoLogo.png'
@@ -7,6 +7,16 @@ import lupa from './img/lupaLogo.png'
 import logoHeader from './img/logoHeader.png'
 
 const Header = () => {
+  const [showMenuMobile, setShowMenuMobile] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowMenuMobile(!showMenuMobile);
+  }
+
+  const closeMenu = () => {
+    setShowMenuMobile(false)
+  }
+
   return (
     <header className='header'> 
         <div className='logo'>
@@ -29,7 +39,31 @@ const Header = () => {
                 <li> <a href="#carrinho"> <img src={carrinho} alt="logoCarrinho" /> </a> </li>
             </ul>
          </nav>
-        
+        {!showMenuMobile && (
+        <div className='menu-mobile' onClick={handleMenuClick}>
+          <div className='line1'></div>
+          <div className='line2'></div>
+          <div className='line3'></div>
+        </div>
+        )}
+
+          <nav className={`mobile-nav ${showMenuMobile ? 'open' : 'closed'}`}>
+              <div className='closeMenuDiv'>
+                <button onClick={closeMenu} className='closeMenuButton'>X</button>
+              </div> 
+              <ul className='linksMobile'>
+                  <li> <a href="#home"> Home </a> </li>
+                  <li> <a href="#shop"> Shop </a> </li>
+                  <li> <a href="#about"> About </a> </li>
+                  <li> <a href="#contact"> Contact </a> </li>
+              </ul>
+              <ul className='imgsMobile'>
+                  <li> <a href="#login"> <img src={login} alt="loginLogo" /> </a> </li>
+                  <li> <a href="#lupa"> <img src={lupa} alt="logoLupa" /> </a> </li>
+                  <li> <a href="#coracao"> <img src={coracao} /> </a> </li>
+                  <li> <a href="#carrinho"> <img src={carrinho} alt="logoCarrinho" /> </a> </li>
+              </ul>
+         </nav>
     </header>
   )
 }
