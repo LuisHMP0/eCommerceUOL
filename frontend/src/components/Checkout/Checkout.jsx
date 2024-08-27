801eef0ce6120097ac2f4039fad50338def9571b
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2024-08-27 05:27:01 -0300

    style: checkout styling

--- frontend/src/components/Checkout/Checkout.jsx
+++ frontend/src/components/Checkout/Checkout.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Checkout.css'
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useRef } from 'react';
 
 const Checkout = () => {
     const [formData, setFormData] = useState({
@@ -15,7 +17,19 @@ const Checkout = () => {
         addOnAddress: '',
         emailAddress: '',
         additionalInfo: '',
-      });
+    });
+    const cartItems = useSelector((state) => state.cart.items);
+    const [activeId, setActiveId] = useState(null);
+
+    const handleClick = (id) => {
+      setActiveId(id === activeId ? null : id); 
+    };
+
+    const formRef = useRef()
+
+    const total = cartItems.reduce((acc, item) => {
+        return acc + item.price * item.quantity;
+      }, 0);
 
     const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,6 +40,9 @@ const Checkout = () => {
     };
 
     const handleSubmit = async (e) => {
+        if (formRef.current) {
+            formRef.current.submit()
+        }
         e.preventDefault();
     
         const token = localStorage.getItem('token');
@@ -50,6 +67,7 @@ const Checkout = () => {
   };
 
   return (
+    <>
     <section className='checkoutContainer'>
         <div className='billingDetailsContainer'>
             <h2> Billing details </h2>
@@ -58,60 +76,94 @@ const Checkout = () => {
 
                     <div className='firstNameDiv'>
                         <label className='firstName' htmlFor='firstName'>First Name</label>
-                        <input 
-                        type='text'
-                        name='firstName'
-                        className='firstNameInput'
-                        value={formData.firstName}
-                        onChange={handleChange}
-                        />
+                        <input type='text' name='firstName' className='firstNameInput' value={formData.firstName} onChange={handleChange} />
                     </div>
 
                     <div className='lastNameDiv'>
                         <label className='lastName' htmlFor='lastName'>Last Name</label>
-                        <input 
-                        type='text'
-                        name='lastName'
-                        className='lastNameInput'
-                        value={formData.lastName}
-                        onChange={handleChange}
-                        />
+                        <input type='text'name='lastName'className='lastNameInput'value={formData.lastName} onChange={handleChange} />
                     </div>
                 </div>
 
                 <label htmlFor='companyName'>Company Name Optional</label>
-                <input type='text' name='companyName' value={formData.companyName} onChange={handleChange}/>
+                <input className='inputRest' type='text' name='companyName' value={formData.companyName} onChange={handleChange}/>
 
                 <label htmlFor='zipCode'>ZIP code</label>
-                <input type='text' name='zipCode' value={formData.zipCode} onChange={handleChange} />
+                <input className='inputRest' type='text' name='zipCode' value={formData.zipCode} onChange={handleChange} />
 
                 <label htmlFor='country'>Country / Region</label>
-                <input type='text' name='country' value={formData.country} onChange={handleChange} />
+                <input className='inputRest' type='text' name='country' value={formData.country} onChange={handleChange} />
 
                 <label htmlFor='streetAddress'>Street address</label>
-                <input type='text' name='streetAddress' value={formData.streetAddress} onChange={handleChange}/>
+                <input className='inputRest' type='text' name='streetAddress' value={formData.streetAddress} onChange={handleChange}/>
 
                 <label htmlFor='city'>Town / City</label>
-                <input type='text' name='city' value={formData.city} onChange={handleChange}/>
+                <input className='inputRest' type='text' name='city' value={formData.city} onChange={handleChange}/>
 
                 <label htmlFor='province'>Province</label>
-                <input type='text' name='province' value={formData.province} onChange={handleChange}/>
+                <input className='inputRest' type='text' name='province' value={formData.province} onChange={handleChange}/>
 
                 <label htmlFor='addOnAddress'>Add-on address</label>
-                <input type='text' name='addOnAddress' value={formData.addOnAddress} onChange={handleChange}/>
+                <input className='inputRest' type='text' name='addOnAddress' value={formData.addOnAddress} onChange={handleChange}/>
 
                 <label htmlFor='emailAddress'>Email address</label>
-                <input type='email' name='emailAddress' value={formData.email} onChange={handleChange}/>
+                <input className='inputRest' type='email' name='emailAddress' value={formData.email} onChange={handleChange}/>
 
                 <label htmlFor='additionalInfo'></label>
-                <input type='text' name='additionalInfo' placeholder='Additional information' value={formData.additionalInfo} onChange={handleChange}/>
+                <input className='inputRest' type='text' name='additionalInfo' placeholder='Additional information' value={formData.additionalInfo} onChange={handleChange}/>
 
-                <button className='makeCheckout' type='submit'> Submit </button>
             </form>
         </div>
         <div className='cartCheckoutContainer'>
+
+                    <> 
+                    <div className='cartCheckout'>
+                        <p> Product </p>
+                        <p> Subtotal </p>
+                    </div>
+                    {cartItems.map(item => (
+                    <div className='productsCheckout'>
+                        <p className='titleProductCheckout'> {item.title} </p>
+                        <p className='subtotalProductCheckout'> Rp. {item.price} </p>
+                    </div>
+                    ))}
+                    <div className='subtotalNoDiscount'>
+                        <p>Subtotal</p>
+                        <p className='subNoDiscount'> Rp. {total} </p>
+                    </div>
+                    <div className='totalCheckout'>
+                        <p className=''> Total </p>
+                        <p className='priceTotalCheckout'> Rp. {total} </p>
+                    </div>
+                    </>
+
+            <div className='paymentMethod'>
+
+                <div className='paymentMethod01'>
+                    <button className={`ball ${activeId === 'paymentMethod01' ? 'active' : ''}`} onClick={() => handleClick('paymentMethod01')}></button>
+                    <p>Direct Bank Transfer</p>
+                </div>
+                {activeId === 'paymentMethod01' && ( <p className='descriptionPayment'>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p> )}
+
+                <div className='paymentMethod02'>
+                    <button className={`ball ${activeId === 'paymentMethod02' ? 'active' : ''}`} onClick={() => handleClick('paymentMethod02')}></button>
+                    <p>Direct Bank Transfer</p>
+                </div>
+                {activeId === 'paymentMethod02' && ( <p className='descriptionPayment'>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p> )}
+
+                <div className='paymentMethod03'>
+                    <button className={`ball ${activeId === 'paymentMethod03' ? 'active' : ''}`} onClick={() => handleClick('paymentMethod03')}></button>
+                    <p>Cash On Delivery</p>
+                </div>
+                {activeId === 'paymentMethod03' && ( <p className='descriptionPayment'>Choose the Cash On Delivery option to pay at the time of delivery. Please ensure you have the exact amount ready, as our delivery personnel may not carry change. Your order will be processed and shipped promptly, and payment will be collected when the product arrives at your address.</p> )}
+
+                <p className='pPrivacyPolices'>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our <span className='privacyPolice'>privacy policy.</span> </p>
+                <button className='makeCheckout' onClick={handleSubmit}> Make Payment </button>
+            </div>
         </div>
+
     </section>
+    </>
   )
 }
 
