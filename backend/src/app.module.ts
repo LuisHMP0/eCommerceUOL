6c4ef5870c49ac4ae2c885d59c6164d68f274ea0
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2024-08-28 16:00:51 -0300

    chore: adding nestjs/config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,10 +7,13 @@ import { join } from 'path'
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CheckoutModule } from './checkout/checkout.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
   providers: [PrismaService], 
   imports: [
+    ConfigModule.forRoot(),
     ProductModule,
     PrismaModule,
     ServeStaticModule.forRoot({
@@ -20,7 +23,15 @@ import { CheckoutModule } from './checkout/checkout.module';
     UserModule,
     AuthModule,
     CheckoutModule,
-  ]
+    JwtModule.registerAsync({
+      imports: [ConfigModule], 
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 
 export class AppModule {}
